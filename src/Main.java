ec10292d6fae92ff48f3a6a69612aa3371955655
al18087 <k-noda1999@outlook.jp>
2025-12-29 19:59:14 +0900

    actionsでjavaを動かす

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+// src/Main.java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub Actions!");
+        System.out.println("yes yes yes");
+    }
+}
